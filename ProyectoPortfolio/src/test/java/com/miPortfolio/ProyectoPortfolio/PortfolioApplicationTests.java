db0b209ea598f6565c66b59dbd1ed56c975ad508
Matías Blanco Stuker <94650427+Lord-Char@users.noreply.github.com>
2023-04-19 19:29:57 -0300

    Update seccion footer
    Intento de mejorar el footer, sin resultados visibles.

---
+++ ProyectoPortfolio/src/test/java/com/miPortfolio/ProyectoPortfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miPortfolio.ProyectoPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
