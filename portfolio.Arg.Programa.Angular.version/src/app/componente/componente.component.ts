f4e263a2a497584d74b2364ac9f936054fe49cb7
Matías Blanco Stuker <94650427+Lord-Char@users.noreply.github.com>
2023-04-24 19:42:51 -0300

    Componentes y estética
    Se agregó un componente más, a modo prueba. En este componente se intenta acceder al index, sin resultados. Se corrige un poco la estética donde se intenta ajustar un poco más la página del intro.

---
+++ portfolio.Arg.Programa.Angular.version/src/app/componente/componente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente',
+  templateUrl: './componente.component.html',
+  styleUrls: ['./componente.component.css']
+})
+export class ComponenteComponent {
+
+}
