8a2b94fd273c3d1b04b70e39dc2e604c3747e365
Matías Blanco Stuker <94650427+Lord-Char@users.noreply.github.com>
2023-04-25 20:07:21 -0300

    Probando el postman [2da. parte]
    Jugando un poco mas con el postman, se prueba el @PathVariable y el @RequestParam.

--- ProyectoPortfolio/src/main/java/com/miPortfolio/ProyectoPortfolio/Controller/Controller.java
+++ ProyectoPortfolio/src/main/java/com/miPortfolio/ProyectoPortfolio/Controller/Controller.java
@@ -3,13 +3,23 @@ package com.miPortfolio.ProyectoPortfolio.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class Controller {
-                        //coincide con
-    @GetMapping ("/hola/{nombre}")               //este
-    public String holaMundo(@PathVariable String nombre){
-        return "Hola Mundo " + nombre;
+                        
+    @GetMapping ("/hola/{nombre}/{apellido}/{edad}")
+    public String holaMundo(@PathVariable String nombre,
+            @PathVariable String apellido,
+            @PathVariable String edad){
+        return "Hola:  " + nombre + " apellido: " + apellido + " edad: " + edad;
+    }
+    
+    @GetMapping ("/chau")
+    public String decirChau(@RequestParam String nombre,
+            @RequestParam String apellido,
+            @RequestParam String edad){
+        return "Chau:  " + nombre + " apellido: " + apellido + " edad: " + edad;
     }
     
 }
