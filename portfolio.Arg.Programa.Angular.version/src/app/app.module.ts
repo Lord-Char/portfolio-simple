8670d1aaf09aa786e6b44592ec65a5afbcdacc0e
Matías Blanco Stuker <94650427+Lord-Char@users.noreply.github.com>
2023-05-11 19:50:01 -0300

    2da. Version en Angular
    De acuerdo a la masterclass del módulo 8, se empieza a realizar nuevamente el proyecto en Angular. Se crea el componente del login (al que yo llamo intro en el proyecto) y se corrigen detalles de estética. Se modifican las rutas de los archivos de estilos y se crea la carpeta de imágenes, que por ahora solo tiene la del logo de Argentina Programa.

--- portfolio.Arg.Programa.Angular.version/src/app/app.module.ts
+++ portfolio.Arg.Programa.Angular.version/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IniciarSesionComponent
   ],
   imports: [
     BrowserModule,
