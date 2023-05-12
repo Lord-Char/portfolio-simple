7ee077a2a32ad015e7c6e9eca47e60690180755c
Matías Blanco Stuker <94650427+Lord-Char@users.noreply.github.com>
2023-05-12 18:17:05 -0300

    Creacion de componentes (Angular version)
    Se crean los siguientes componentes, buscando la conexion entre el componente router y los componentes portfolio e iniciar sesion
    -creacion componente portfolio
    -creacion componente index-portfolio, que ira dentro del componente portfolio
    Esta dando errores en la compilacion del elemento index-portfolio

--- portfolio.Arg.Programa.Angular.version/src/app/app.module.ts
+++ portfolio.Arg.Programa.Angular.version/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { IndexPortfolioComponent } from './componentes/index-portfolio/index-portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IniciarSesionComponent
+    IniciarSesionComponent,
+    PortfolioComponent,
+    IndexPortfolioComponent
   ],
   imports: [
     BrowserModule,
---
+++ portfolio.Arg.Programa.Angular.version/src/app/componentes/index-portfolio/index-portfolio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-portfolio',
+  templateUrl: './index-portfolio.component.html',
+  styleUrls: ['./index-portfolio.component.css']
+})
+export class IndexPortfolioComponent {
+
+}
